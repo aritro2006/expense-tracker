2521fb149c8d86cf2dc1c9381340d84995c0cbb4
aritro2006 <aritroghosh2906@gmail.com>
2026-03-13 19:47:22 +0530

    fixed mongoose model overwrite error

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -2,26 +2,30 @@ const mongoose = require("mongoose");
 
 const TransactionSchema = new mongoose.Schema({
 
-user:{
-type:mongoose.Schema.Types.ObjectId,
-ref:"User"
-},
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
 
-text:{
-type:String,
-required:true
-},
+  text:{
+    type:String,
+    required:true
+  },
 
-amount:{
-type:Number,
-required:true
-},
+  amount:{
+    type:Number,
+    required:true
+  },
 
-date:{
-type:Date,
-default:Date.now
-}
+  date:{
+    type:Date,
+    default:Date.now
+  }
 
 });
 
-module.exports = mongoose.model("Transaction",TransactionSchema);
+/* IMPORTANT FIX */
+
+module.exports =
+mongoose.models.Transaction ||
+mongoose.model("Transaction", TransactionSchema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,22 +2,24 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
 
-name:{
-type:String,
-required:true
-},
+  name:{
+    type:String,
+    required:true
+  },
 
-email:{
-type:String,
-required:true,
-unique:true
-},
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
 
-password:{
-type:String,
-required:true
-}
+  password:{
+    type:String,
+    required:true
+  }
 
 });
 
-module.exports = mongoose.model("User",UserSchema);
+module.exports =
+mongoose.models.User ||
+mongoose.model("User", UserSchema);
